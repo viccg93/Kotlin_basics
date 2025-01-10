41ebae81770dbdc0cc6dd21a48fb63b1565a8285
Vic CG <vic.cg93@gmail.com>
2025-01-10 04:18:42 -0600

    constructores

--- classes_objects.kt
+++ classes_objects.kt
@@ -1,4 +1,9 @@
 //las clases en kotlin se definen de la siguiente manera
+//en kotlin existen 2 tipos de constructores, un unico que es el primario y se indica en la definicion de class
+//y se pueden definir n o ningun constructor secundario que estan en el cuerpo de la clase
+//cuando no se define explicitamente el constructor primario, el compilador lo crea
+//para ser consistentes cuando el compilador primario no recibe ningun parametro ni hay anotaciones, se omite la keyword constructor
+//y tambien se omiten los parentesis como en la clase SmartDevice
 class SmartDevice {
     //cuerpo de la clase
     //las porpiedades pueden ser mutables o inmutables como las "variables"
@@ -44,6 +49,31 @@ class SmartDevice {
     }
 }
 
+//el constructor primario recibe 2 parametros, en primero es la propiedad brandName
+//que ya no se encuentra inicializada en el cuerpo, ya que la funcion del constructor primario es inicializar las propiedades
+//la propiedad model si se encuentra en el cuerpo y no se inicializa en el constructor primario
+    class Laptop constructor ( val brandName: String){
+        var model = "00000000"
+        get()= field
+        set(value){
+            //aqui se garantiza que cualquier asignación a model posterior a la construccion cumpla con la longitud adecuada
+            if(value.length == 8){
+                field = value
+            }else{
+                field = "00000000"
+            }
+        }
+        //todos los constructores secundarios deben llamar al constructor primario y pasar los parametros necesarios
+        //aqui se garantiza la inicialización de model con la longitud adecuada
+        constructor(brandName: String, model: String): this(brandName){
+            //al tener el mismo nombre la propiedad y el parametro, se usa this
+            this.model = when(model.length){
+                8->model
+                else->"00000000"
+            }
+        }
+    }
+
 fun main(){
     println("clases y objetos en Kotlin")
     //asi se crea una instancia en Kotlin
@@ -65,4 +95,10 @@ fun main(){
     //el valor de volumen no se modifica debido a la condicional que se encuentra en el setter
     myDevice.speakerVolume = 200
     println("el volumen de ${myDevice.name} es ${myDevice.speakerVolume}")
+    
+    //cuando instanciamos Laptop, la propiedad brandName se asigna directamente, mientras que model pasa un procesamiento
+    val myLaptop = Laptop(brandName = "Huawei", model = "0123")
+    println("laptop ${myLaptop.brandName}, modelo: ${myLaptop.model}")
+    myLaptop.model = "12345678"
+    println("laptop ${myLaptop.brandName}, modelo: ${myLaptop.model}")
 }
