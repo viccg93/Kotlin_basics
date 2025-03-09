33376b21f61804f89f4193db1bf078a3f73bae59
Vic CG <vic.cg93@gmail.com>
2025-03-08 18:36:35 -0600

    propiedades y metodos extension

--- property_extension.kt
+++ property_extension.kt
@@ -1,7 +1,7 @@
-
 fun main(){
     //estas propiedades son accesibles desde Quiz porque estan marcadas como companion
-    println("${Quiz.answered} of ${Quiz.total} answered")
+    //println(Quiz.progressText)
+    Quiz.printProgressBar()
 }
 
 
@@ -21,6 +21,26 @@ class Quiz {
         var total: Int = 10
         var answered: Int = 3
     }
+}
+
+//para definir extensiones se debe de hacer usando el nombre de la clase
+//ademas se debe de asignar a un val o var
+//el tip debe de estar definido explicitamente
+//las propiedades extension no pueden almacenar valores, por lo que no se pueden usar setters
+val Quiz.StudentProgress.progressText: String
+//es getter es identico como si la propiedad se hubiera definido de la clase
+//answered y total son propiedades del objeto companion StudentProgress
+get() = "${answered} of ${total} answered"
+
+//tambien se pueden definir funciones extension, basta solo indicar
+//el nombre del tipo debe antes del nombre de la funcion
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒")}
+    println("")
+    //se puede acceder a una propiedad extension desde una funcion extension
+    println(Quiz.progressText)
 }
 
 
